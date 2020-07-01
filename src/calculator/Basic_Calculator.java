693c54479a961291dfd22aff0bc7b00d03782e1c
Easir Aeafat <riyadhbd2@gmail.com>
2020-07-01 19:21:33 +0200

    Basic Calculator in Java Program

---
+++ src/calculator/Basic_Calculator.java
@@ -0,0 +1,52 @@
+
+package calculator;
+
+import java.util.Scanner;
+
+
+public class Basic_Calculator {
+    public static void main(String[] args) {
+        double a,b;
+        char c;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first number");
+        a = input.nextDouble();
+        System.out.println("Enter operator,+,-,*,/");
+        c = input.next().charAt(0);
+        System.out.println("Enter second number");
+        b = input.nextDouble();
+        
+        double result;
+        switch(c){
+            case '+':
+                result  = a + b;
+                System.out.println(a + b);
+                break;
+            case '-':
+                result = a - b;
+                System.out.println(a - b);
+                break;
+            case '*':
+                result = a * b;
+                System.out.println(a * b);
+                break;
+            case '/':
+                result = a / b;
+                System.out.println(a / b);
+                break;
+            default:
+                System.out.println("You have entered wrong information");
+        
+            
+        }
+        
+        
+        
+        
+    }
+    
+}
+
+    }
+    
+}
